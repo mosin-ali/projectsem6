45df2cc119c6c2ecc1b8e7438362ccc5ff9377a0
mosin-ail <manasiyamosinali1@gmail.com>
2025-01-02 12:32:45 +0530

    1.0

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <link href="css/style.css" rel="stylesheet">
+ <body>
+ <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
+        <a href="index.html" class="navbar-brand p-0">
+            <h1 class="m-0 text-primary"><i class="fa fa-tooth me-2"></i>Dental Health care</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="mainhome.php" class="nav-item nav-link">Home</a>
+                <a href="about.php" class="nav-item nav-link">About</a>
+                <a href="doctor.php" class="nav-item nav-link">Doctor</a>
+                <a href="service.php" class="nav-item nav-link">Service</a>
+                <a href="contact.php" class="nav-item nav-link">Contact</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="bi bi-person-circle"></i> User</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="#">Patient</a><br>
+                        <a href="#">Doctor</a><br>
+                        <a href="#">Admin</a>
+                    </div>
+                </div>
+            </div>
+            <a href="appointment.php" class="btn btn-primary py-2 px-4 ms-3">Appointment</a>
+        </div>
+    </nav>
+ </body>
+ </html>
---
+++ mainhome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ <?php   include("csslink.php") ?>
+</head>
+<body>
+<?php   include("topbar.php") ?> 
+<?php   include("header.php") ?> 
+<?php   include("slider.php") ?> 
+<?php   include("banner.php") ?>
+<?php   include("price.php") ?>
+<?php   include("review.php") ?> 
+<?php   include("offer.php") ?>
+<?php   include("footer.php") ?> 
+<?php include("jsslink.php") ?>
+</body>
+</html>
